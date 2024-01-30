7089358b54496cc41fdfd96d4c29e85983a2b209
umitd111 <umit.demirci1996@gmail.com>
2024-01-31 01:46:42 +0300

    React router installed. Basics are set

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,33 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Home from "./pages/home/home";
+import Order from "./pages/home/order/order";
+import Error from "./pages/error/error";
+import AppLayout from "./layouts/appLayout";
+import Login from "./pages/login/login";
+
+const router = createBrowserRouter([
+  {
+    element: <AppLayout />,
+    errorElement: <Error />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/order",
+        element: <Order />,
+      },
+    ],
+  },
+  {
+    path: "/login",
+    element: <Login />,
+  },
+]);
+
 function App() {
-  return <h1>Hello</h1>;
+  return <RouterProvider router={router} />;
 }
 
 export default App;
